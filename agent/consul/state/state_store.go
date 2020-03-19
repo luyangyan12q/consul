8ad8b198cceca2aa79ebaddf38f731c0c3fa8cb9
Paul Banks <banks@banksco.de>
2020-03-19 13:11:20 +0000

    state: track changes so that they may be used to produce change events

--- agent/consul/state/state_store.go
+++ agent/consul/state/state_store.go
@@ -3,8 +3,9 @@ package state
 import (
 	"errors"
 	"fmt"
+
 	"github.com/hashicorp/consul/agent/structs"
-	"github.com/hashicorp/go-memdb"
+	memdb "github.com/hashicorp/go-memdb"
 )
 
 var (
@@ -97,7 +98,7 @@ const (
 // from the Raft log through the FSM.
 type Store struct {
 	schema *memdb.DBSchema
-	db     *memdb.MemDB
+	db     *memDBWrapper
 
 	// abandonCh is used to signal watchers that this state store has been
 	// abandoned (usually during a restore). This is only ever closed.
@@ -108,13 +109,15 @@ type Store struct {
 
 	// lockDelay holds expiration times for locks associated with keys.
 	lockDelay *Delay
+
+	// TODO: publisher *EventPublisher
 }
 
 // Snapshot is used to provide a point-in-time snapshot. It
 // works by starting a read transaction against the whole state store.
 type Snapshot struct {
 	store     *Store
-	tx        *memdb.Txn
+	tx        *txnWrapper
 	lastIndex uint64
 }
 
@@ -122,7 +125,7 @@ type Snapshot struct {
 // data to a state store.
 type Restore struct {
 	store *Store
-	tx    *memdb.Txn
+	tx    *txnWrapper
 }
 
 // IndexEntry keeps a record of the last index per-table.
@@ -155,11 +158,14 @@ func NewStateStore(gc *TombstoneGC) (*Store, error) {
 	// Create and return the state store.
 	s := &Store{
 		schema:       schema,
-		db:           db,
 		abandonCh:    make(chan struct{}),
 		kvsGraveyard: NewGraveyard(gc),
 		lockDelay:    NewDelay(),
 	}
+	s.db = &memDBWrapper{
+		MemDB: db,
+	}
+	// TODO: s.publisher = NewEventPublisher(s, 0, 10*time.Second)
 	return s, nil
 }
 
@@ -206,7 +212,7 @@ func (s *Snapshot) Close() {
 // the state store. It works by doing all the restores inside of a single
 // transaction.
 func (s *Store) Restore() *Restore {
-	tx := s.db.Txn(true)
+	tx := s.db.WriteTxnRestore()
 	return &Restore{s, tx}
 }
 
@@ -234,6 +240,34 @@ func (s *Store) Abandon() {
 	close(s.abandonCh)
 }
 
+// ComputeIndex returns the latest index across all tables.
+func (s *Store) ComputeIndex() (uint64, error) {
+	tx := s.db.Txn(false)
+	defer tx.Abort()
+	idx, err := s.computeIndexTxn(tx)
+	if err != nil {
+		return 0, err
+	}
+
+	return idx, nil
+}
+
+// computeIndexTxn returns the latest index across all tables.
+func (s *Store) computeIndexTxn(tx *txnWrapper) (uint64, error) {
+	iter, err := tx.Get("index", "id")
+	if err != nil {
+		return 0, err
+	}
+	highestIndex := uint64(0)
+	for index := iter.Next(); index != nil; index = iter.Next() {
+		if idx, ok := index.(*IndexEntry); ok && idx.Value > highestIndex {
+			highestIndex = idx.Value
+		}
+	}
+
+	return highestIndex, nil
+}
+
 // maxIndex is a helper used to retrieve the highest known index
 // amongst a set of tables in the db.
 func (s *Store) maxIndex(tables ...string) uint64 {
@@ -244,11 +278,11 @@ func (s *Store) maxIndex(tables ...string) uint64 {
 
 // maxIndexTxn is a helper used to retrieve the highest known index
 // amongst a set of tables in the db.
-func maxIndexTxn(tx *memdb.Txn, tables ...string) uint64 {
+func maxIndexTxn(tx *txnWrapper, tables ...string) uint64 {
 	return maxIndexWatchTxn(tx, nil, tables...)
 }
 
-func maxIndexWatchTxn(tx *memdb.Txn, ws memdb.WatchSet, tables ...string) uint64 {
+func maxIndexWatchTxn(tx *txnWrapper, ws memdb.WatchSet, tables ...string) uint64 {
 	var lindex uint64
 	for _, table := range tables {
 		ch, ti, err := tx.FirstWatch("index", "id", table)
@@ -265,7 +299,7 @@ func maxIndexWatchTxn(tx *memdb.Txn, ws memdb.WatchSet, tables ...string) uint64
 
 // indexUpdateMaxTxn is used when restoring entries and sets the table's index to
 // the given idx only if it's greater than the current index.
-func indexUpdateMaxTxn(tx *memdb.Txn, idx uint64, table string) error {
+func indexUpdateMaxTxn(tx *txnWrapper, idx uint64, table string) error {
 	ti, err := tx.First("index", "id", table)
 	if err != nil {
 		return fmt.Errorf("failed to retrieve existing index: %s", err)
